43196eabb3bcd6357a7055abd1790bfc69becc7e
Henry Tran <htran48@uwo.ca>
2017-09-17 03:15:59 -0400

    update meal route

--- server/api.js
+++ server/api.js
@@ -46,20 +46,21 @@ router.route('/login')
 router.route('/meal')
     // Fetch a meal based on the user's nutrition profile
     .post(function(req, res) {
-        let ingredient = req.body.ingredients;
         User.findById(req.body.userId, function(err, user) {
             if (!user) {
                 user = "";
             }
 
+            let getUrl = EDAMAM_URL + 'q=' + req.body.ingredients + '&app_id=' + APP_ID + '&app_key=' + APP_KEY 
+                         + '&calories=lte%20' + req.body.calories + '&to=1';
+
             request.get(
-                EDAMAM_URL + 'q=' + ingredient + '&app_id=' + APP_ID + '&app_key=' + APP_KEY 
-                + '&calories=lte%20' + user.remainingCalories + '&to=1',
+                getUrl,
                 function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         let recipe = JSON.parse(body).hits[0].recipe;
                         //Subtract remaining calories with recipe
-                        user.remainingCalories -= Integer.parseInt(recipe.calories);
+                        user.remainingCalories -= parseInt(recipe.calories);
 
                         res.send(recipe);
                     }
@@ -76,16 +77,17 @@ router.route('/user/:id/nutrition')
             
 
             // Use the Harris-Benedict equation to calculate user's BMR and caloric requirement
-            if (user.gender === "male") {
+            if (req.gender === "male") {
                 user.totalCalories = ( (10 * req.weight) + (6.25 * user.height) - (5 * user.age) + 5 ) * req.activity
             } else {
                 user.totalCalories = ( (10 * req.weight) + (6.25 * user.height) - (5 * user.age) - 161 ) * req.activity
             }
 
             // Adjust calories based on the users goals
-            if (user.goals === "gain") user.totalCalories += 200;
-            else if (user.goals === "lose") user.totalCalories -= 200;
+            if (req.goals === "gain") user.totalCalories += 200;
+            else if (req.goals === "lose") user.totalCalories -= 200;
 
+            user.save()
             return res.json({User: user});
         })
     })
