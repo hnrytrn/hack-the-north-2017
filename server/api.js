c636ced367c1d031e296a2f25851ebc7ec552de7
Henry Tran <htran48@uwo.ca>
2017-09-16 23:00:45 -0400

    add meal endpoint

--- server/api.js
+++ server/api.js
@@ -3,6 +3,12 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
+var request = require('request');
+
+// Used for the recipe API
+const APP_ID = "f6c0736d";
+const APP_KEY = "640e60b4821cfee24b35ab6743008a97";
+
 router.route('/user')
     // Create new user
     .post(function(req, res) {
@@ -14,6 +20,12 @@ router.route('/user')
     }); 
 
 router.route('/user/:id')
+    .get(function(res, req) {
+        User.findById(req.params.id, function(err, user) {
+            if (err) response.send(err);
+            return res.json({User: user});
+        })
+    })
     // Update existing user
     .put(function(req, res) {
         User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
@@ -28,6 +40,30 @@ router.route('/login')
             if (err) response.send(err);
             return res.json({User: user});
         })
-    })
+    });
+
+router.route('/meal')
+    .post(function(req, res) {
+        let ingredient = req.body.ingredients;
+        User.findById(req.body.userId, function(err, user) {
+            if (!user) {
+                user = "";
+            }
+
+            request.get(
+                'https://api.edamam.com/search?q=' + ingredient + '&app_id=' + APP_ID + '&app_key=' + APP_KEY 
+                + '&calories=lte%20' + user.remainingCalories + '&to=1',
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        let recipe = JSON.parse(body).hits[0].recipe;
+                        //Subtract remaining calories with recipe
+                        user.remainingCalories -= Integer.parseInt(recipe.calories);
+
+                        res.send(recipe);
+                    }
+                }
+            );
+        });
+    });
 
 module.exports = router;
--- server/models/models.js
+++ server/models/models.js
@@ -5,8 +5,8 @@ var userSchema = new mongoose.Schema({
     lastName: String,
     email: String,
     password: String,
-    goal: String,
-    dietType: String,
+    goal: String, 
+    dietType: String,   //“balanced”, “high-protein”, “high-fiber”, “low-fat”, “low-carb”, “low-sodium”
     gender: String,
     weight: Number,
     totalCalories: Number,
