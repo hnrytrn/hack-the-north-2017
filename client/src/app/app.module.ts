5dcd350b09737c2f5f749ac96f65b945ef3b569d
Gautham Padmakumar <gpadmaku@gmail.com>
2017-09-16 23:21:46 -0400

    finished landing and created login/register components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
