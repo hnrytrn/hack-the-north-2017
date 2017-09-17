cfbe05cb778115880061bac2d0270479917faa97
Gautham Padmakumar <gpadmaku@gmail.com>
2017-09-17 02:59:52 -0400

    kinda broken

--- client/src/app/sign-in/sign-in.component.ts
+++ client/src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../_services/user.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
+  providers: [UserService]
 })
 export class SignInComponent implements OnInit {
+  username: String;
+  password: String;
+  constructor(userService: UserService){ 
 
-  constructor() { }
-
+  }
   ngOnInit() {
   }
 
+  signIn() {
+    userService.login(this.username, this.password)
+      .subscribe(
+        user => 
+      )
+    }
 }
