ee46490b23536140c6a3e85a734dc3ed19864672
Henry Tran <htran48@uwo.ca>
2017-09-16 23:01:28 -0400

    add routing module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { UserService } from './_services/user.service';
+import { ChatComponent } from './chat/chat.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
